b557da58a5c09cb850e0de39b0dfe81a41de6846
woncheolok <okcleff@gmail.com>
2024-01-08 22:50:37 +0900

    feat: add nav bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '../globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+
+import '../globals.css';
+import TopBar from '@/components/shared/TopBar';
+import LeftSideBar from '@/components/shared/LeftSideBar';
+import RightSideBar from '@/components/shared/RightSideBar';
+import BottomBar from '@/components/shared/BottomBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +23,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <TopBar />
+
+          <main className='flex flex-row'>
+            <LeftSideBar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSideBar />
+          </main>
+
+          <BottomBar />
+        </body>
       </html>
     </ClerkProvider>
   );
