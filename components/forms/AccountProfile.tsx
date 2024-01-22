5a99ffd92683b3c873724471ff7d4a94befeb7ea
woncheolok <okcleff@gmail.com>
2024-01-22 22:02:10 +0900

    feat: init Onboarding

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,16 @@
+interface AccountProfileProps {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
+  return <div>AccountProfile</div>;
+};
+export default AccountProfile;
