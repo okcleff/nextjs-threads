aeb03a0f61d9bb70fced3d482bba45a1e7964940
okcleff <okcleff@gmail.com>
2024-02-01 10:32:42 +0900

    feat: add fetch posts function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
-import { UserButton } from '@clerk/nextjs';
+import { fetchPosts } from "@/lib/actions/thread.actions";
 
-export default function Home() {
+async function Home() {
+  const postsResponse = await fetchPosts(1);
+  console.log(postsResponse);
+
   return (
     <>
-      <h1 className='head-text text-left'>Home</h1>
+      <h1 className="head-text text-left">Home</h1>
     </>
   );
 }
+
+export default Home;
