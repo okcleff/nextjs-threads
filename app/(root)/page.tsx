3796401b773c20ba7d411293fe5058aa8c3e706a
okcleff <okcleff@gmail.com>
2024-02-01 10:57:31 +0900

    feat: add thread card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,38 @@
-import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+import { fetchThreads } from "@/lib/actions/thread.actions";
+import ThreadCard from "@/components/cards/ThreadCard";
 
 async function Home() {
-  const postsResponse = await fetchPosts(1);
-  console.log(postsResponse);
+  const user = await currentUser();
+  if (!user) return null;
+
+  const postsResponse = await fetchThreads(1);
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {postsResponse.threads.length === 0 ? (
+          <p className="no-result">No thread found</p>
+        ) : (
+          <>
+            {postsResponse.threads.map((thread) => (
+              <ThreadCard
+                key={thread._id}
+                id={thread._id}
+                currentUserId={thread.id}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -7,14 +7,14 @@ import User from "../models/user.model";
 import Thread from "../models/thread.model";
 import Community from "../models/community.model";
 
-export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
   connectToDB();
 
   // Calculate the number of posts to skip based on the page number and page size.
   const skipAmount = (pageNumber - 1) * pageSize;
 
   // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
-  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+  const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
     .sort({ createdAt: "desc" })
     .skip(skipAmount)
     .limit(pageSize)
@@ -36,15 +36,15 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
     });
 
   // Count the total number of top-level posts (threads) i.e., threads that are not comments.
-  const totalPostsCount = await Thread.countDocuments({
+  const totalThreadsCount = await Thread.countDocuments({
     parentId: { $in: [null, undefined] },
   }); // Get the total count of posts
 
-  const posts = await postsQuery.exec();
+  const threads = await threadsQuery.exec();
 
-  const isNext = totalPostsCount > skipAmount + posts.length;
+  const isNext = totalThreadsCount > skipAmount + threads.length;
 
-  return { posts, isNext };
+  return { threads, isNext };
 }
 
 interface ICreateThreadParams {
